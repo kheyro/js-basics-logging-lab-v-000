3471654e3ced95be072f8afb0fb0872f36915d54
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 10:44:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const driver = 'bob';
 
 console.log('this code was called')
-console.log(bob)
-console.log(bob, 'is the driver variable name')
+console.log(driver)
+console.log(driver, 'is the driver variable name')
